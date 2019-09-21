5db1ff87ae0642e78fb6bab3e48316fac30a1ce5
Haidar Hendri Setyawan <haidarhendri7@gmail.com>
2019-09-21 13:54:35 +0700

    detil apps

--- app/src/main/java/uns/belanja/DetailProductActivity.kt
+++ app/src/main/java/uns/belanja/DetailProductActivity.kt
@@ -2,11 +2,30 @@ package uns.belanja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_detil_product.*
 
 class DetailProductActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_PRICE = "extra_price"
+        const val EXTRA_IMAGE_URL = "extra_image_url"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detil_product)
+        showProductDetail()
+    }
+
+    private fun showProductDetail(){
+        etProductNameDetail.setText(intent.getStringExtra(EXTRA_NAME))
+        etHargaProduct.setText(intent.getStringExtra(EXTRA_PRICE))
+        etGambarProduct.setText(intent.getStringExtra(EXTRA_IMAGE_URL))
+
+        Glide.with(this)
+            .load(intent.getStringExtra(EXTRA_IMAGE_URL))
+            .into(imgProductDetail)
     }
 }
--- app/src/main/java/uns/belanja/MainActivity.kt
+++ app/src/main/java/uns/belanja/MainActivity.kt
@@ -1,5 +1,6 @@
 package uns.belanja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,20 +12,51 @@ import uns.belanja.model.ProductsData
 class MainActivity : AppCompatActivity() {
 
     private var list: ArrayList<Products> = arrayListOf()
+    private lateinit var listProductAdapter: ListProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showListProducts()
+        setListClickAction()
+        setupAddProduct()
     }
 
     private fun showListProducts() {
         list.addAll(ProductsData.listProduct)
+        listProductAdapter = ListProductAdapter(list)
+
         rvProducts.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = ListProductAdapter(list)
+            adapter = listProductAdapter
             setHasFixedSize(true)
         }
+    }
+
+    private fun setupAddProduct(){
+        btnAddProduct.setOnClickListener {
+            val detailIntent = Intent(
+                this,
+                DetailProductActivity::class.java
+            )
+            startActivity(detailIntent)
+        }
+    }
+
+    private fun setListClickAction(){
+        listProductAdapter.setOnItemClickCallback(
+            object : ListProductAdapter.OnItemClickCallback{
+                override fun onItemClick(data: Products) {
+                    val manageDetailIntent = Intent(this@MainActivity,
+                        DetailProductActivity::class.java).apply{
+                        putExtra(DetailProductActivity.EXTRA_NAME, data.name)
+                        putExtra(DetailProductActivity.EXTRA_IMAGE_URL, data.image)
+                        putExtra(DetailProductActivity.EXTRA_PRICE, data.price.toString())
+                    }
+                    startActivity(manageDetailIntent)
+                }
+            }
+        )
     }
 }
